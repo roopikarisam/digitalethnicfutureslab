d67da0989e9036a589ac062af95a65f23cf5e694
Matthew Currie <matthew.d.currie.24@dartmouth.edu>
2023-10-18 14:43:47 -0400

    Adding programs and data to github

---
+++ region_finder.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+
+# Specify the directory containing the CSV files for each region
+data_directory = r"C:\Users\mc505\Downloads\College_Info_Program\Data_Directory"
+
+
+# list of colleges
+colleges_to_search = ["California Institute of Technology","Harvard University","Columbia University","Massachusetts Institute of Technology","Stanford University","Brown University","Curtis Institute of Music","University of Chicago","Yale University","Dartmouth College","Duke University","Princeton University","Amherst College","Cornell University","Johns Hopkins University","Northeastern University","Northwestern University","Pomona College","Swarthmore College","University of Pennsylvania","Vanderbilt University","Colby College","Williams College","Barnard College","Bowdoin College","Johnson C. Smith University","Rice University","University of California, Los Angeles","Claremont McKenna College","Juilliard School","Tufts University","Carnegie Mellon University","Emory University","Grinnell College","Tulane University","United States Naval Academy","University of California, Berkeley","Washington University in St. Louis","Colgate University","Georgetown University","Hamilton College","New York University","St. Andrews University","United States Military Academy at West Point","University of Southern California","Alice Lloyd College","Harvey Mudd College","Middlebury College","Pacific Oaks College","United States Air Force Academy","University of Notre Dame","Bates College","Boston University","Colorado College","Faulkner University","Haverford College","Wellesley College","Wesleyan University","United States Coast Guard Academy","Boston College","Carleton College","Davidson College","Georgia Institute of Technology","Harris-Stowe State University","University of North Carolina at Chapel Hill","Washington and Lee University","Pitzer College","University of Michigan--Ann Arbor","Franklin W. Olin College of Engineering","Rhode Island School of Design","University of Miami","University of Virginia","Vassar College","College of the Ozarks","Hillsdale College","University of California, Irvine","Wake Forest University","Webb Institute","Babson College","Cooper Union for the Advancement of Science and Art","Denison University","Smith College","University of Florida","Villanova University","University of California, San Diego","University of Richmond","Berea College","Florida State University","Kettering College","Skidmore College","University of California, Santa Barbara","University of Tampa","Case Western Reserve University","Macalester College","Scripps College","Spelman College","California Institute of the Arts","Thomas University","United States Merchant Marine Academy","Tuskegee University"]
+# results dictionary
+results = {}
+
+# loop through each region (csv file) and check for matches
+for filename in os.listdir(data_directory):
+    if filename.endswith(".csv"):
+        region = os.path.splitext(filename)[0]  # region name extracted from file name
+        file_path = os.path.join(data_directory, filename)
+
+        # read file into df
+        df = pd.read_csv(file_path)
+
+        
+        for college in colleges_to_search:
+            # regex to match strings
+            matching_colleges = df[df['Name'].str.contains(college, case=False, na=False, regex=False)]
+
+            # add matches
+            if not matching_colleges.empty:
+                for _, row in matching_colleges.iterrows():
+                    results[college] = {
+                        'Region': region,
+                        'Campus Size': row['Campus setting']
+                    }
+
+
+for college, info in results.items():
+    print(f"College: {college}, Region: {info['Region']}, Campus Size: {info['Campus Size']}")
+
+print(len(results))
+
+df = pd.DataFrame.from_dict(results, orient="index")
+
+csv_file_path = "locations_results.csv"
+
+#save to csv
+df.to_csv(csv_file_path)
+
+print(f"Results saved to {csv_file_path}")
---
+++ statement_to_csv.py
@@ -0,0 +1,79 @@
+## PULLED FROM GOOGLE SHEETS API QUICKSTART
+
+from __future__ import print_function
+
+import os
+import csv
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1y3tv2tQB9bNBCungTh_Zb3zK2yUMPGEG9YQDi0qpDUM"
+SAMPLE_RANGE_NAME = 'Top 100 - Lowest Acceptance Rate!A2:L100'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        # Create a directory to store CSV files
+        if not os.path.exists('csv_files'):
+            os.makedirs('csv_files')
+
+        for row in values:
+            if len(row) >= 5:
+                value_in_column_a = row[0]
+                value_in_column_e = row[4]
+                if value_in_column_a and value_in_column_e:
+                    # Generate a CSV file name based on the value in column A
+                    csv_file_name = os.path.join('csv_files', f'{value_in_column_a}.csv')
+                    # Write the value from column E to the CSV file
+                    with open(csv_file_name, 'w', newline='', encoding='utf-8') as csv_file:
+                        csv_writer = csv.writer(csv_file)
+                        csv_writer.writerow([value_in_column_e])
+
+        print('CSV files created.')
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
