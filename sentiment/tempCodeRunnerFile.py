026d772a32c1bc5069de425a000227e2c0ddabe5
Matthew Currie <97186925+mattdcurrie@users.noreply.github.com>
2023-11-01 14:03:13 -0400

    Delete sentiment/tempCodeRunnerFile.py

--- sentiment/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-def calculate_sentiment(text):
-    # Run VADER on the text
-    scores = sentimentAnalyser.polarity_scores(text)
-    # Extract the compound score
-    compound_score = scores['compound']
-    # Return compound score
-    return compound_score
